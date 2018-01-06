410b7fffe89143e24eee44bac7e2fef3b3481135
Yi-Hua Liang <yihua1218@gmail.com>
2018-01-06 10:37:55 +0800

    Fixed vue2-google-maps yarn run generate issue.

--- starter/nuxt.config.js
+++ starter/nuxt.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   /*
   ** Headers of the page
@@ -9,35 +11,18 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    script: [
-      { src: 'https://apis.google.com/js/api:client.js' }
-    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/retina.js/2.1.3/retina.js' },
+      { src: 'https://apis.google.com/js/api:client.js' }
     ]
   },
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  },
   modules: [
     'bootstrap-vue/nuxt',
     '@nuxtjs/font-awesome'
@@ -51,5 +36,28 @@ module.exports = {
     google_app_client_id: process.env.GOOGLE_APP_CLIENT_ID || '',
     google_map_api_key: process.env.GOOGLE_MAP_API_KEI || '',
     facebook_app_id: process.env.FACEBOOK_APP_ID || ''
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.splice(0, 0, {
+          test: /\.js$/,
+          include: [path.resolve(__dirname, './node_modules/vue2-google-maps')],
+          loader: 'babel-loader'
+        })
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
